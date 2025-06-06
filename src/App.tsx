86a1c479cc91f7005c88111765d49407907a6e13
syedalthaf786 <syedalthaf760.com>
2025-06-06 12:17:58 +0530

    "publish"

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/AcademiPredict">
+      <BrowserRouter basename="/academic-predict">
         <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
